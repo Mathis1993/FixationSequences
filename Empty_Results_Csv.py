edcce3d4a5244f9c7a5511eae4c14f745f85e9e2
Mathis1993 <m_eric01@uni-muenster.de>
2019-04-08 10:54:34 -0400

    added n_epochs

--- Empty_Results_Csv.py
+++ Empty_Results_Csv.py
@@ -1,4 +1,4 @@
 import pandas as pd
 #create empty df to store the results and store it to disk
-results = pd.DataFrame(columns = ["batch_size", "learning_rate", "mean_accuracy_per_image", "mean_test_loss", "mean_validation_loss", "mean_train_loss", "number_of_hits", "number_of_test_images", "number_of_fixations"])
+results = pd.DataFrame(columns = ["batch_size", "n_epochs", "learning_rate", "mean_accuracy_per_image", "mean_test_loss", "mean_validation_loss", "mean_train_loss", "number_of_hits", "number_of_test_images", "number_of_fixations"])
 results.to_csv("results.csv", index=False, header=True)
